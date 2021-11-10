ffc97098a17fdc38b3d942796610511b64232101
mar1n <30738216+mar1n@users.noreply.github.com>
2021-11-10 23:06:57 +0000

    eslint configuration done

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,48 @@
 module.exports = {
   env: {
-    browser: true,
+    node: true,
     commonjs: true,
     es2021: true,
   },
   extends: [
     'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 13,
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint', 'eslint-comments', 'import', 'prettier'],
   rules: {
+    'prettier/prettier': 'error',
+    'import/extensions': 'off',
+    'no-console': 'off',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'never',
+        groups: [
+          ['builtin', 'external'],
+          ['internal', 'parent', 'sibling', 'index'],
+        ],
+      },
+    ],
   },
+  setting: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      }
+    }
+  }
 };
